095633e0f0bfdf811393a8c33715eb3bf41750a2
Rimil Dey <rimildeyjsr@gmail.com>
2018-03-07 15:02:08 +0530

    got a collection view to work

---
+++ WeatherApp/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Rimil Dey on 06/03/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+  
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+}
---
+++ WeatherApp/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  WeatherApp
+//
+//  Created by Rimil Dey on 06/03/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import Foundation
+
+let model = Model()
+
+class Model {
+    let labelsData = [
+        "hello","nice to meet you","hi"
+    ]
+}
